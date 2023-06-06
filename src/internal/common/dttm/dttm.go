fc5a576c8452a2fd9a78d5fe31266652f490e105
Abin <mail@meain.io>
2023-06-06 18:59:12 +0530

    Fix incorrect time format for M365DateTimeTimeZone

--- src/internal/common/dttm/dttm.go
+++ src/internal/common/dttm/dttm.go
@@ -37,11 +37,11 @@ const (
 
 	// SafeForTesting is used for testing restore destination folders.
 	// Microsecond granularity prevents collisions in parallel package or workflow runs.
-	SafeForTesting TimeFormat = HumanReadableDriveItem + ".000000"
+	SafeForTesting TimeFormat = HumanReadableDriveItem + ".0000000"
 
 	// M365dateTimeTimeZoneTimeFormat is the format used by M365 for datetimetimezone resource
 	// https://learn.microsoft.com/en-us/graph/api/resources/datetimetimezone?view=graph-rest-1.0
-	M365DateTimeTimeZone TimeFormat = "2006-01-02T15:04:05.000000"
+	M365DateTimeTimeZone TimeFormat = "2006-01-02T15:04:05.0000000"
 )
 
 // these regexes are used to extract time formats from strings.  Their primary purpose is to
